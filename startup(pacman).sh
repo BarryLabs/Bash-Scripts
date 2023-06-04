6621345c08a55bf768f86a959c6bde245ca129df
Chandler <93629088+BarryLabs@users.noreply.github.com>
2023-06-04 01:54:37 +0000

    Update startup(pacman).sh

--- startup(pacman).sh
+++ startup(pacman).sh
@@ -6,7 +6,7 @@
 sudo pacman -Syu
 # Sync the database, clean up orphanced packages and remove all extra configuration files.
 wait
-sudo pacman -Scc -y
+sudo pacman -Scc
 #Clear the cache.
 wait
 paccache -r
