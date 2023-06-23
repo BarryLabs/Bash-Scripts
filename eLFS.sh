dd23b2ace3c094d6477dfb886a5d451600509918
Chandler <93629088+BarryLabs@users.noreply.github.com>
2023-06-23 03:21:39 +0000

    Update eLFS.sh

--- eLFS.sh
+++ eLFS.sh
@@ -2,7 +2,7 @@
 
 # Check for appropriate script flags.
 if [ $# -eq 0 ]; then
-	echo "You must provide the..."
+	echo "You must provide -e/-d as the first flag, the path as the second and your password as the third."
 	exit 0
 fi
 
