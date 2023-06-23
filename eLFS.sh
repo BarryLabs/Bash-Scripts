65e0051b2fe6259f997ebe4c8c470acd8fdbc097
Chandler <93629088+BarryLabs@users.noreply.github.com>
2023-06-23 03:22:58 +0000

    Update eLFS.sh

--- eLFS.sh
+++ eLFS.sh
@@ -62,46 +62,46 @@ case $1 in
         cd $directory
         openssl enc -d -aes-256-cbc -pbkdf2 -pass pass:$password -in $foldername -out $foldername.tar.gz
         tar -xzf $foldername.tar.gz
-		rm $foldername.tar.gz
-		rm $foldername
+	rm $foldername.tar.gz
+	rm $foldername
 
-		cd "${foldername%.*}"
-		echo "D travels down folders, U travels up one folder, C cat's a file or Q allows you to Quit."
+	cd "${foldername%.*}"
+	echo "D travels down folders, U travels up one folder, C cat's a file or Q allows you to Quit."
 
-		while :
-		do
-			echo "Current directory:  $(pwd)"
-			ls
-			echo -n "Input: "
-			read INPUT
+	while :
+	do
+		echo "Current directory:  $(pwd)"
+		ls
+		echo -n "Input: "
+		read INPUT
 
-			case $INPUT in
-				U)
-					cd ..
-				;;
-				C)
-					read -p "File to cat: " cFile
-					cat $cFile
-				;;
-				D)
-					echo -n "Enter folder name: "
-					read dirname
-					cd $dirname
-					ls
-				;;
-				Q)
-					echo "Exiting..."
-					cd $directory
-					tar -czf ${foldername%.*}.tar.gz ${foldername%.*}
-					openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -in ${foldername%.*}.tar.gz -out ${foldername%.*}.enc
-					rm ${foldername%.*}.tar.gz
-					rm -rf ${foldername%.*}
-					exit 0
-				;;
-				*)
-					echo "Invalid Input."
-				;;
-			esac
-		done
+		case $INPUT in
+			U)
+				cd ..
+			;;
+			C)
+				read -p "File to cat: " cFile
+				cat $cFile
+			;;
+			D)
+				echo -n "Enter folder name: "
+				read dirname
+				cd $dirname
+				ls
+			;;
+			Q)
+				echo "Exiting..."
+				cd $directory
+				tar -czf ${foldername%.*}.tar.gz ${foldername%.*}
+				openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -in ${foldername%.*}.tar.gz -out ${foldername%.*}.enc
+				rm ${foldername%.*}.tar.gz
+				rm -rf ${foldername%.*}
+				exit 0
+			;;
+			*)
+				echo "Invalid Input."
+			;;
+		esac
+	done
     ;;
 esac
