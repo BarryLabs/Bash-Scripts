dcb5e386203b7862be9feb07b21b99e6ceda1af8
Chandler <93629088+BarryLabs@users.noreply.github.com>
2023-06-23 01:54:41 +0000

    Update startup(pacman).sh

--- startup(pacman).sh
+++ startup(pacman).sh
@@ -4,12 +4,9 @@
 
 # Sync the database. 
 sudo pacman -Syu
-# Sync the database, clean up orphanced packages and remove all extra configuration files.
 wait
 sudo pacman -Scc
-#Clear the cache.
 wait
 paccache -r
-# Print message.
 wait
-echo "System has been updated, databases have been synced and caches have been cleared."
+echo "Update complete."
